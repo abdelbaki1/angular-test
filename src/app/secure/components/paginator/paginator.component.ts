b10e6d096ba7f01f99d636495fea2cbf80725bb9
abdelbaki1 <chriguiabdelbaki@gmail.com>
2022-10-30 15:45:29 +0100

    Doc:18 correction du pagination

--- src/app/secure/components/paginator/paginator.component.ts
+++ src/app/secure/components/paginator/paginator.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import { last } from 'rxjs/operators';
 
 @Component({
   selector: 'app-paginator',
@@ -6,9 +7,10 @@ import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges
   styleUrls: ['./paginator.component.css']
 })
 export class PaginatorComponent implements OnInit,OnChanges {
-  @Input() lastPage: number ;
+  @Input() lastPage: number; ;
   @Output() pageChanged = new EventEmitter<number>();
   page = 1;
+  page_number:number;
   page_list=[]
   constructor() {
   }
@@ -18,16 +20,24 @@ export class PaginatorComponent implements OnInit,OnChanges {
     for (var i = 0; i < this.lastPage; i++) {
       this.page_list.push(i+1);
     }
+    this.page_number=Math.round(this.lastPage/2);
+    console.log(this.page_number);
   }
+
   }
   ngOnInit(): void {
+   
+    
   }
 
   next(number): void {
     if (number > this.lastPage || number < 1 || number == this.page) {
       return;
     }
+    
+    
     this.page=number;
     this.pageChanged.emit(number);
+    console.log(this.page,this.page_list,this.page_list.slice(this.page-1,this.page+this.page_number));
   }
 }
